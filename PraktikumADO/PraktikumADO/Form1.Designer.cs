eff561d07fe7170c114a52ed7ad2471961575974
FazaViranza <201728547+FazaViranza@users.noreply.github.com>
2026-03-14 04:07:11 +0700

    Mengganti Property Button Ketigga Menjadi HitungMK

--- PraktikumADO/PraktikumADO/Form1.Designer.cs
+++ PraktikumADO/PraktikumADO/Form1.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.btnConnect = new System.Windows.Forms.Button();
             this.btnHitungMhs = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnHitungMK = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnConnect
@@ -51,21 +51,21 @@
             this.btnHitungMhs.Text = "Hitung Mhs";
             this.btnHitungMhs.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnHitungMK
             // 
-            this.button1.Location = new System.Drawing.Point(435, 344);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnHitungMK.Location = new System.Drawing.Point(435, 344);
+            this.btnHitungMK.Name = "btnHitungMK";
+            this.btnHitungMK.Size = new System.Drawing.Size(75, 23);
+            this.btnHitungMK.TabIndex = 2;
+            this.btnHitungMK.Text = "Hitung Mk";
+            this.btnHitungMK.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnHitungMK);
             this.Controls.Add(this.btnHitungMhs);
             this.Controls.Add(this.btnConnect);
             this.Name = "Form1";
@@ -78,7 +78,7 @@
 
         private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.Button btnHitungMhs;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnHitungMK;
     }
 }
 
