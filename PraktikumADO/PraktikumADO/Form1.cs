fee5d04d2bcfff7e4f280285fdabbe6e7cbd63e6
FazaViranza <201728547+FazaViranza@users.noreply.github.com>
2026-03-14 04:33:45 +0700

    Menggunakan ExecuteNonQuery pada button update
    ExecuteNonQuery digunakan untuk menjalankan query yang tidak menghasilkan data. ExecuteNonQuery mengembalikan jumlah baris yang terpengaruh oleh query.

--- PraktikumADO/PraktikumADO/Form1.cs
+++ PraktikumADO/PraktikumADO/Form1.cs
@@ -101,7 +101,25 @@ namespace PraktikumADO
 
         private void btnUpdate_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Koneksi();
+                conn.Open();
 
+                string query = "UPDATE Mahasiswa SET Alamat = 'Yogyakarta' WHERE NIM = '20240140132'";
+
+                cmd = new SqlCommand(query, conn);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Jumlah baris terpengaruh : " + hasil);
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
